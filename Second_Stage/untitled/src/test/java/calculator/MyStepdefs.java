f204f7c4d78d2db467c7ddb3faa8aa15e35b5728
PoriaKH <poria.gm@gmail.com>
2025-04-28 20:01:00 +0330

    adding scenarios

---
+++ Second_Stage/untitled/src/test/java/calculator/MyStepdefs.java
@@ -0,0 +1,32 @@
+package calculator;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class MyStepdefs {
+    @Given("the first number is {int}")
+    public void theFirstNumberIs(int arg0) {
+    }
+
+    @And("the second number is {int}")
+    public void theSecondNumberIs(int arg0) {
+    }
+
+    @When("I multiply the two numbers")
+    public void iMultiplyTheTwoNumbers() {
+    }
+
+    @Then("the result should be {int}")
+    public void theResultShouldBe(int arg0) {
+    }
+
+    @When("I divide the first number by the second number")
+    public void iDivideTheFirstNumberByTheSecondNumber() {
+    }
+
+    @When("I raise the first number to the power of the second number")
+    public void iRaiseTheFirstNumberToThePowerOfTheSecondNumber() {
+    }
+}
---
+++ Second_Stage/untitled/src/test/resources/features/calculator.feature
@@ -0,0 +1,20 @@
+Feature: Calculator
+  We want to perform production (multiplication), division, and power operations.
+
+  Scenario: Multiply two numbers
+    Given the first number is 6
+    And the second number is 2
+    When I multiply the two numbers
+    Then the result should be 12
+
+  Scenario: Divide two numbers
+    Given the first number is 6
+    And the second number is 2
+    When I divide the first number by the second number
+    Then the result should be 3
+
+  Scenario: Power of two numbers
+    Given the first number is 6
+    And the second number is 2
+    When I raise the first number to the power of the second number
+    Then the result should be 36
